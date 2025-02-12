d60cd90ed931e94e2568f68b60fc3dbc1f96f889
Gilles Gonzalez Oropeza <gilles.gonzalezoropeza@gmail.com>
2025-02-12 20:32:18 +0100

    Drag and drop working

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,8 +1,34 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { DragControls } from 'three/examples/jsm/controls/DragControls';
+import { Vector3 } from 'three';
 
-export function setupControls(camera, domElement) {
+export function setupCameraControls(camera, domElement) {
     const controls = new OrbitControls(camera, domElement);
     controls.enableDamping = true; // Ajoute un amortissement pour une meilleure expérience utilisateur
     controls.dampingFactor = 0.05;
+    return controls;
+}
+
+let moving_particle = null;
+export function setupDragControls(particles, camera, domElement) {
+    const controls = new DragControls(particles.map(p => p.mesh), camera, domElement);
+    controls.addEventListener('dragstart', function (event) {
+        // Change color of selected particle
+        moving_particle = particles.find(p => p.mesh === event.object);
+        moving_particle.mesh.material.color.setHex(Math.random() * 0xffffff);
+    });
+    
+    controls.addEventListener('drag', function (event) {
+        // Move the particle
+        if (moving_particle) {
+            const newPosition = new Vector3();
+            newPosition.copy(event.object.position);
+            moving_particle.physicsBody.position.set(newPosition.x, newPosition.y, newPosition.z);
+        }
+    });
+    
+    controls.addEventListener('dragend', function (event) {
+        moving_particle = null;
+    });
     return controls;
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createScene } from './components/Scene';
 import { createCamera } from './components/Camera';
 import { createRenderer } from './components/Renderer';
 import { addLights } from './components/Lights';
-import { setupControls } from './components/Controls';
+import { setupCameraControls, setupDragControls } from './components/Controls';
 import { handleResize } from './utils/ResizeHandler';
 import Monitor from './utils/Monitor';
 import * as THREE from "three";
@@ -18,25 +18,16 @@ const scene = createScene();
 const camera = createCamera();
 const renderer = createRenderer();
 const monitor = new Monitor();
-const mouse = new THREE.Vector2();
-let prev_mouse = new THREE.Vector2();
-let mouse_down = false;
-let moving_particle = null;
-let raycaster = new THREE.Raycaster();
 
 document.body.appendChild(renderer.domElement);
 
 // Ajout de lumières
 addLights(scene);
 
-
 // Load GLTF model using GLTFModelLoader class
 const modelLoader = new GLTFModelLoader('./src/assets/GLTF/scene.gltf', scene,'./src/assets/GLTF/textures/Muchkin2_baseColor.png');
 modelLoader.applyTexture(scene);
 modelLoader.loadModel();
-
-// Ajout des contrôles
-const controls = setupControls(camera, renderer.domElement);
 
 // Setup physics world
 const world = new CANNON.World();
@@ -69,65 +60,21 @@ for (let i = 0; i < 50; i++) {
     particles.push(particule);
 }
 
-// Toggle camera controls on / off
+// Contrôles de déplacement des particules
+const dragControls = setupDragControls(particles, camera, renderer.domElement);
+dragControls.enabled = false;
+
+// Contrôles de la caméra
+const camControls = setupCameraControls(camera, renderer.domElement);
+camControls.enabled = true;
+
+// Key to switch between camera and drag controls
 document.addEventListener('keydown', (event) => {
-    if (event.key === 'c') {
-        controls.enabled = !controls.enabled;
+    if (event.key === 'c' || event.key === 'C') {
+        camControls.enabled = !camControls.enabled;
+        dragControls.enabled = !dragControls.enabled;
     }
 });
-
-// Events for particles translation
-document.addEventListener( 'mousedown', onDocumentMouseDown );
-document.addEventListener( 'mouseup', onDocumentMouseUp );
-document.addEventListener( 'mousemove', onDocumentMouseMove );
-function onDocumentMouseDown( event ) {
-    event.preventDefault();
-    if (controls.enabled) return;
-    mouse_down = true;
-
-    prev_mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    prev_mouse.y = -((event.clientY / window.innerHeight) * 2 - 1);
-
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-    mouse.y = -((event.clientY / window.innerHeight) * 2 - 1)
-    
-    raycaster.setFromCamera(mouse, camera)
-
-    let imax = -1;
-    let lmax = 0;
-    for (let i = 0; i < particles.length; i++) {
-        const hits = raycaster.intersectObject(particles[i].mesh);
-        if (hits.length) {
-            if (hits[0].distance < lmax) continue;
-            imax = i;
-            lmax = hits[0].distance;
-        }
-    }
-    if (imax === -1) return;
-    moving_particle = particles[imax]
-    moving_particle.mesh.material.color.setHex(Math.random() * 0xffffff);
-}
-function onDocumentMouseUp( event ) {
-    event.preventDefault();
-    if (controls.enabled) return;
-    mouse_down = false;
-}
-function onDocumentMouseMove( event ) {    
-    event.preventDefault();
-    if (!mouse_down || controls.enabled || !moving_particle) return;
-
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1
-    mouse.y = -((event.clientY / window.innerHeight) * 2 - 1)
-    
-    let translation = new THREE.Vector3();
-
-    // Needs to be fixed
-    translation.subVectors(moving_particle.mesh.position, camera.position);
-    translation.normalize();
-    translation.multiplyScalar((mouse.x - prev_mouse.x) * translation.length());
-    moving_particle.physicsBody.position = moving_particle.physicsBody.position.vadd(new CANNON.Vec3(translation.x, translation.y, translation.z));
-    
-}
 
 // Animation
 function animate() {
